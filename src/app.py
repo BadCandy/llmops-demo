165c5b03520e467ed66a3008b6d5fb1f5addcb51
flex-yuncheol <flex-yuncheol@flex.team>
2025-04-27 22:17:17 +0900

    feature: Add LLMOps Evaluation History

--- src/app.py
+++ src/app.py
@@ -3,13 +3,13 @@ import streamlit as st
 testing = st.Page("testing.py", title="Testing")
 dataset = st.Page("dataset.py", title="Dataset")
 evaluation = st.Page("evaluation.py", title="Evaluation")
-# evaluation_history = st.Page("evaluation_history.py", title="Evaluation History")
+evaluation_history = st.Page("evaluation_history.py", title="Evaluation History")
 
 pg = st.navigation([
     testing,
     dataset,
     evaluation,
-    # evaluation_history,
+    evaluation_history,
 ])
 
 pg.run()
